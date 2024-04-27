7dce87fbcc675dbfa61f96bc0b9c1ffb92e2712b
RamazanNurov <149464187+RamazanNurov@users.noreply.github.com>
2024-04-27 14:29:14 +0300

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,5 +1,5 @@
 import sqlite3 as sq
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 from tkinter.ttk import *
 
@@ -24,7 +24,7 @@ class Connect_db:
 
 class My_window:
     def __init__(self):
-        self.win = Tk()
+        self.win = tk.Tk()
         self.win.geometry('900x400')
         self.win.title('СТО')
         self.create_frames()
@@ -36,16 +36,16 @@ class My_window:
         style = Style()
 
         style.configure('TFrame0', background='lightblue')
-        self.notebook.pack(expand=True, fill=BOTH)
+        self.notebook.pack(expand=True, fill=tk.BOTH)
 
 
         self.frame1 = Frame(self.notebook)
         self.frame2 = Frame(self.notebook)
         self.frame3 = Frame(self.notebook)
 
-        self.frame1.pack(fill=BOTH, expand=True)
-        self.frame2.pack(fill=BOTH, expand=True)
-        self.frame3.pack(fill=BOTH, expand=True)
+        self.frame1.pack(fill=tk.BOTH, expand=True)
+        self.frame2.pack(fill=tk.BOTH, expand=True)
+        self.frame3.pack(fill=tk.BOTH, expand=True)
 
         self.notebook.add(self.frame1, text="Товары")
         self.notebook.add(self.frame2, text="Купить")
@@ -63,27 +63,27 @@ class My_window:
         self.table_tov.column('price', width=150, anchor='c')
         self.table_tov.place(x=10, y=10)
 
-        self.tovar_name = StringVar()
+        self.tovar_name = tk.StringVar()
         self.lb_name = Label(self.frame1, text='Наименование товара', font='Arial 12', background='lightblue')
         self.lb_name.place(x = 350, y = 20)
 
-        self.entry_name = Entry(self.frame1, textvariable=self.tovar_name, font='Arial 12')
+        self.entry_name = tk.Entry(self.frame1, textvariable=self.tovar_name, font='Arial 12')
         self.entry_name.place(x=350, y = 60)
 
-        self.tovar_price = DoubleVar()
-        self.lb_name = Label(self.frame1, text='Цена товара', font='Arial 12', background='lightblue')
+        self.tovar_price = tk.DoubleVar()
+        self.lb_name = tk.Label(self.frame1, text='Цена товара', font='Arial 12', background='lightblue')
         self.lb_name.place(x = 350, y = 100)
 
-        self.entry_price = Entry(self.frame1, textvariable=self.tovar_price, font='Arial 12')
+        self.entry_price = tk.Entry(self.frame1, textvariable=self.tovar_price, font='Arial 12')
         self.entry_price.place(x=350, y = 140)
 
-        self.btn_new_tov = Button(self.frame1, text='Добавить новый товар')
+        self.btn_new_tov = tk.Button(self.frame1, text='Добавить новый товар',fg='lightblue')
         self.btn_new_tov.place(x=600, y=60)
 
-        self.btn_delete_tov = Button(self.frame1, text='Удалить товар')
+        self.btn_delete_tov = tk.Button(self.frame1, text='Удалить товар')
         self.btn_delete_tov.place(x=600, y=100)
 
-        self.btn_update_tov = Button(self.frame1, text='Изменить товар')
+        self.btn_update_tov = tk.Button(self.frame1, text='Изменить товар')
         self.btn_update_tov.place(x=600, y=140)
 
     def buy(self):
